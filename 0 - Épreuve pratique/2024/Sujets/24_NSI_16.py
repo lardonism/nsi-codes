1ca97f60b7a837e65d100ef5bb0354f3ac7b1040
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-04-27 13:05:32 +0200

    Exo 1 test

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_16.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_16.py
@@ -1,3 +1,19 @@
+def dec_to_bin(dec):
+    """ int -> bin
+        passe un decimal en binaire 
+    """
+    binaire = ""
+    while dec != 0:
+        binaire += str(dec%2)
+        dec = dec//2
+    binaire = [elm for elm in binaire]
+    binaire = [binaire[(len(binaire)-1) - i ] for i in range(len(binaire))]
+    binaire = "".join(binaire)
+    return binaire
+
+
+
+
 def echange(tab, i, j):
     '''Echange les éléments d'indice i et j dans le tableau tab.'''
     temp = ... 
