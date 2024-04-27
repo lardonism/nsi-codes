6d634da5d6e22f4262a829e97ff198a293ca543b
Ogorek_Kiszony <159456782+Ogorek2912@users.noreply.github.com>
2024-04-27 17:25:03 +0200

    Update 24_NSI_19.py

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_19.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_19.py
@@ -1,3 +1,12 @@
+def liste_puissances(a,n,borne=None):
+        def puissance(a,n):
+                result = 1
+                for _ in range(n):
+                        result *= a
+                        return result
+        if borne is None:
+                return [puissance(a,i) for i in range(i,borne) if abs(puissance(a,i)) < abs(borne)]
+
 dico = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
         "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
         "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17,
@@ -11,12 +20,12 @@ def codes_parfait(mot):
     - code_concatene est le code des lettres du mot concaténées ;
     - mot_est_parfait est un booléen indiquant si le mot est parfait."""
     code_concatene = ""
-    code_additionne = ... 
+    code_additionne = 0 
     for c in mot:
-        code_concatene = code_concatene + ... 
-        code_additionne = code_additionne + ... 
+        code_concatene = code_concatene + str(ord(c))
+        code_additionne = code_additionne +  ord(c)
     code_concatene = int(code_concatene)
-    mot_est_parfait = ... 
+    mot_est_parfait = (len(set(mot)) == len(mot))
     return code_additionne, code_concatene, mot_est_parfait
 
 
