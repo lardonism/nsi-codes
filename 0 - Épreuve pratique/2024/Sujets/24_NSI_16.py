3a59af87ab40775cf44d35626223b743d470c535
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-04-27 13:30:16 +0200

    Fin exo

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_16.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_16.py
@@ -20,17 +20,17 @@ binaire = [elm for elm in binaire]
 
 def echange(tab, i, j):
     '''Echange les éléments d'indice i et j dans le tableau tab.'''
-    temp = ... 
-    tab[i] = ... 
-    tab[j] = ... 
+    temp = tab[i]
+    tab[i] = tab[j]
+    tab[j] = temp
 
 def tri_bulles(tab):
     '''Trie le tableau tab dans l'ordre croissant
     par la méthode du tri à bulles.'''
     n = len(tab)
-    for i in range(...): 
-        for j in range(...): 
-            if ... > ...: 
-                echange(tab, j, ...) 
+    for i in range(len(tab)): 
+        for j in range((len(tab)-i)-1): 
+            if tab[j] > tab[j+1]: 
+                echange(tab, j, j+1)
 
 
