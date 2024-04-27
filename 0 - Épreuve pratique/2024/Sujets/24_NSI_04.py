d0d64eff4f70ff6e839623998c335b016c7b18a3
Shanyusss <148460900+Shanyusss@users.noreply.github.com>
2024-04-27 15:07:30 +0200

    Update 24_NSI_04.py

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_04.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_04.py
@@ -1,17 +1,22 @@
-def distance_carre(point1, point2):
-    """ Calcule et renvoie la distance au carre entre 
-    deux points."""
-    return (...)**2 + (...)**2 
+def recherche(tab:list,n:int):
+    ido = None
+    for i in range(len(tab)):
+        if tab[i] == n:
+            ido = i
+    return ido
 
-def point_le_plus_proche(depart, tab):
-    """ Renvoie les coordonnées du premier point du tableau tab se 
-    trouvant à la plus courte distance du point depart."""
+def distance_carre(point1,point2):
+    """Calcule et renvoie la distance au carre entre deux points."""
+    return (point1[0]-point2[0])**2+(point1[1]-point2[1])**2
+
+def point_le_plus_proche(depart,tab):
+    """ Renvoie les coordonnées du premier point du tableau tab se trouvant à la plus courte distance du point départ."""
     min_point = tab[0]
-    min_dist = ... 
-    for i in range(1, len(tab)):
-        if distance_carre(tab[i], depart) < ...: 
-            min_point = ... 
-            min_dist = ... 
+    min_dist = distance_carre(min_point, depart) 
+    for i in range(1,len(tab)):
+        if distance_carre(tab[i],depart) < min_dist:
+            min_point = tab[i]
+            min_dist = distance_carre(tab[i],depart)
     return min_point
 
 
