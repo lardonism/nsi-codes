15426529a5526b9a36a3df2febf7f482b8bf8757
Clemencemc <159456909+Clemencemc@users.noreply.github.com>
2024-04-28 22:01:25 +0200

    Correction Exercice 18

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_18.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_18.py
@@ -1,15 +1,23 @@
-def chercher(tab, x, i, j):
+def multiplication(n1, n2): # Exercice 1
+    resultat = 0
+    if n1<0 and n2<0 or n1<0:
+        n1 = -n1
+        n2 = -n2
+    for i in range(n1):
+        resultat += n2
+    return resultat
+
+def chercher(tab, x, i, j): # Exercice 2
     '''Renvoie l'indice de x dans tab, si x est dans tab, 
     None sinon.
     On suppose que tab est trié dans l'ordre croissant.'''
     if i > j:
         return None
-    m = (i + j) // ... 
-    if ... < x: 
-        return chercher(tab, x, ... , ...) 
+    m = (i + j) // 2 
+    if tab[m] < x: 
+        return chercher(tab, x, m , j) 
     elif tab[m] > x:
-        return chercher(tab, x, ... , ...) 
+        return chercher(tab, x, i , m) 
     else:
-        return ... 
-
+        return m 
 
