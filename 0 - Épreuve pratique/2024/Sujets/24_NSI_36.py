a75473fe985a5377d9d829de874476adce56fcaa
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-04-28 15:18:39 +0200

    modif

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_36.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_36.py
@@ -1,13 +1,3 @@
-def nbr_occurrences(chaine):
-    occ = {}
-	   for elm in chaine:
-		     if elm in occ:
-			       occ[elm] +=  1
-		     else: 
-			       occ[elm] = 1
-	   return occ
-
-
 valeurs = [100, 50, 20, 10, 5, 2, 1]
 
 def rendu_glouton(a_rendre, rang):
