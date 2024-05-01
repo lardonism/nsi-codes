a3336dcf4428f4e23b47c3fb3ee29d5c8c51746f
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-05-01 22:07:31 +0200

    tp nuit du code

--- 0 - Nuit du Code/1 - Labyrinthe/App.py
+++ 0 - Nuit du Code/1 - Labyrinthe/App.py
@@ -1,26 +1,55 @@
-import pyxel
+from Personnage import *
+from Labyrinthe import *
+BLOC = 8
+HAUT, BAS, GAUCHE, DROITE = (0, -BLOC), (0, BLOC), (-BLOC, 0), (BLOC, 0)
+
 
 class App:
     """ Application graphique - Pyxel """
+
     def __init__(self):
         """ App -> None """
         # initialisation de la fenêtre graphique
-        pyxel.init(128, 128, title="La Nuit du Code")
+        self.hero = Personnage(8,8)
+        self.commandes = {pyxel.KEY_RIGHT: DROITE,
+                          pyxel.KEY_UP: HAUT,
+                          pyxel.KEY_LEFT: GAUCHE,
+                          pyxel.KEY_DOWN: BAS,
+                          };
+        self.lab = Labyrinthe(51,51)
+        pyxel.init(408, 408, title="La Nuit du Code", fps = 60)
         pyxel.run(self.update, self.draw)
+
+    def exec_btn(self):
+        for cle in self.commandes:
+            if pyxel.btn(cle):
+                self.deplacer(self.commandes[cle])
 
     def update(self):
         """ App -> None
         Met à jour l'application """
+        self.exec_btn()
 
     def draw(self):
         """ App -> None
         Affiche les éléments dans la fenêtre graphique """
+        pyxel.cls(0)
+        self.lab.afficher()
+        self.hero.afficher()
 
-    
-    
-    def deplacer(self, perso, direction):
+    def deplacer(self, direction):
         """ App, Personnage, (int, int) -> None
         Déplace le personnage dans la direction indiquée si cela est possible """
-        pass
+        xp, yp = self.hero.x//BLOC, self.hero.y//BLOC
+        x, y = direction
+        x, y = x//8, y//8
+        if self.lab.est_sol((x+xp,y+yp)):
+            self.hero.x += x
+            self.hero.y += y
+
+
+
+
+
 
 App()
--- 0 - Nuit du Code/1 - Labyrinthe/Personnage.py
+++ 0 - Nuit du Code/1 - Labyrinthe/Personnage.py
@@ -1,5 +1,12 @@
+import pyxel
+
+BLOC = 8
+HAUT, BAS, GAUCHE, DROITE = (0, -BLOC), (0, BLOC), (-BLOC, 0), (BLOC, 0)
+
+
 class Personnage:
     """Un personnage dans le labyrinthe"""
+
     def __init__(self, x=0, y=0):
         """ Personnage -> None """
         self.x = x
@@ -9,9 +16,23 @@ class Personnage:
         """ Personnage, (int, int) -> None
         Met à jour la position du personnage après un
         déplacement de direction = (dx, dy) """
-        pass
-    
-    def afficher(self):
+        x, y = direction
+        self.x += x
+        self.y += y
+
+    def haut(self):
+        self.deplacer(HAUT)
+
+    def bas(self):
+        self.deplacer(BAS)
+
+    def gauche(self):
+        self.deplacer(GAUCHE)
+
+    def droite(self):
+        self.deplacer(DROITE)
+
+    def afficher(self, couleur=5):
         """ Personnage -> None
         Affiche le personnage à l'écran """
-        pass
+        pyxel.rect(self.x, self.y, 8, 8, couleur)
