f1bee9816333fe49f80dfff1dc5ba1a18db72471
lardonism <159458178+lardonism@users.noreply.github.com>
2024-05-03 20:56:30 +0200

    Esteban 24_NSI_10.py
    c'est tres en retard, excuses et supplications

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_10.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_10.py
@@ -1,3 +1,14 @@
+def moyenne(notes):
+    coef = 0
+    note = 0
+    for e in notes:
+        coef = coef+e[1]
+        note = note+e[0]*e[1]
+    if coef == 0:
+        return None
+    else:
+        return note/coef
+
 coeur = [[0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
          [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
          [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
@@ -8,9 +19,8 @@ coeur = [[0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
          [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
-
 def affiche(dessin):
-    ''' affichage d'une grille : les 1 sont représentés par 
+    ''' affichage d'une grille : les 1 sont représentés par
         des "*" , les 0 par un espace " " '''
     for ligne in dessin:
         affichage = ''
@@ -25,20 +35,19 @@ def affiche(dessin):
 def liste_zoom(liste_depart,k):
     '''renvoie une liste contenant k fois chaque élément de
        liste_depart'''
-    liste_zoomee = ... 
-    for elt in ... : 
+    liste_zoomee = []
+    for e in  liste_depart:
         for i in range(k):
-            ...
+            liste_zoomee.append(e)
     return liste_zoomee
 
 def dessin_zoom(grille,k):
-    '''renvoie une grille où les lignes sont zoomées k fois 
+    '''renvoie une grille où les lignes sont zoomées k fois
        ET répétées k fois'''
     grille_zoomee=[]
     for ligne in grille:
-        ligne_zoomee = ... 
+        ligne_zoomee = liste_zoom(ligne,k)
         for i in range(k):
-            ... .append(...) 
+            grille_zoomee.append(ligne_zoomee)
     return grille_zoomee
-
 
